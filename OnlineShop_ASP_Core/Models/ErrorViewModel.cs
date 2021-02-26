88bff8f5d26dfdafdd7363bd927ecd54621f1eb7
ezenity <anthonymmacallister@gmail.com>
2021-02-26 12:00:23 -0500

    Add project files.

---
+++ OnlineShop_ASP_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace OnlineShop_ASP_Core.Models {
+    public class ErrorViewModel {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
