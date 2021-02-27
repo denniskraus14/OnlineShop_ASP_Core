4ba4f5f73a80c90c8d734e0ac6a13fa1eecfc5a9
ezenity <anthonymmacallister@gmail.com>
2021-02-26 21:39:54 -0500

    Feat: Create information and configuration for accessing the database.

---
+++ Entities/Entities/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop_ASP_Core.Models.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop_ASP_Core.Models {
+
+    public class ApplicationContext : IdentityDbContext<User> {
+        public ApplicationContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        //public DbSet<Role> Roles { get; set; }
+
+        //public DbSet<UserRegistrationModel> UserRegistrations { get; set; }
+    }
+}
