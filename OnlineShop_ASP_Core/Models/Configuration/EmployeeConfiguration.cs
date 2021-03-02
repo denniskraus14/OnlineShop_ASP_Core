257815c3fbcfc2178026763f133a2f815761acb2
ezenity <anthonymmacallister@gmail.com>
2021-03-02 11:58:37 -0500

    Refactor: Not Service App but Web App
    - Confused with what the Web App is and how to utilize a Service App

---
+++ OnlineShop_ASP_Core/Models/Configuration/EmployeeConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop_ASP_Core.Models.Configuration {
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>{
+
+        /// <summary>
+        /// Used to seed data to the database
+        /// </summary>
+        /// <param name="builder"></param>
+        public void Configure(EntityTypeBuilder<Employee> builder) {
+            builder.ToTable("Employee");
+            builder.Property(e => e.Age).IsRequired(false);
+            // Can be used to determine the role this employee gets within the website
+            //builder.Property(e => e.IsRegularEmployee).HasDefaultValue(true);
+
+            builder.HasData(
+                new Employee {
+                    //Id = new Guid("0f8fad5b-d9cb-469f-a165-70867728950e"),
+                    Id = Guid.NewGuid(),
+                    Name = "Ant Mac",
+                    Age = 31,
+                    Position = "Software Developer",
+                },
+                new Employee {
+                    //Id = new Guid("7c9e6679-7425-40de-944b-e07fc1f90ae7"),
+                    Id = Guid.NewGuid(),
+                    Name = "Mac Ant",
+                    Age = 31,
+                    Position = "Software Engineer",
+                }
+            );
+        }
+    }
+}
---
+++ OnlineShop_ASP_Core/Models/UserRegistrationModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop_ASP_Core.Models {
+    public class UserRegistrationModel {
+
+        public string FirstName { get; set;  }
+
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and conrfirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
