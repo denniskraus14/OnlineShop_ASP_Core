286a07a65d794557910b3aff2375011b01ad6fd9
Dennis Kraus <dekraus@wesleyan.edu>
2021-03-03 11:41:51 -0500

    Feat: Start of Shop Controller, Portal page

---
+++ OnlineShop_ASP_Core/Controllers/ShopController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop_ASP_Core.Controllers {
+    public class ShopController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Portal() {
+            /*
+             * if(Session["Email"]!=null{
+             *      return View();
+             * }
+             * return RedirectToAction(nameof(AccountController.Login,"Account");
+             */
+            return View(); //until session is set up
+        }
+
+
+    }
+}
